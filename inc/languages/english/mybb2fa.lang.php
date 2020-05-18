6242584333470500969e944dd45048549beb7bf9
Omar Gonzalez <Sama34@users.noreply.github.com>
2020-05-18 16:52:31 -0700

    Fix #1 languages/english/mybb2fa.lang.php spelling error

--- inc/languages/english/mybb2fa.lang.php
+++ inc/languages/english/mybb2fa.lang.php
@@ -1,5 +1,5 @@
 <?php
-$l['mybb2fa_activated'] = "Two Factoc Authentication has been activated. Please scan the QR Code now.";
+$l['mybb2fa_activated'] = "Two Factor Authentication has been activated. Please scan the QR Code now.";
 $l['mybb2fa_loggedin'] = "The code was correct, you're logged in now";
 $l['mybb2fa_failed'] = "The code was incorrect, you're logged out now";
 $l['mybb2fa'] = "MyBB Two Factor Authentication";
